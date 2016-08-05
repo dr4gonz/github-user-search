77556cb21ccfce8a20d99a54322c372c3b56b5b4
Matthew <mreyez@gmail.com>
2016-08-05 09:52:18 -0700

    add user repositories to list and show their information after click in a separate div

--- js/github-search-interface.js
+++ js/github-search-interface.js
@@ -1,12 +1,32 @@
 var GitHubSearch = require('./../js/github-search.js').githubsearchModule;
 
 var displayUser = function(user) {
-  console.log(user);
   $("#found-user-image").html("<img class='user-avatar-thumb' src="+user.avatar_url+"></img>");
   $("#found-user-name").text(user.name);
   $("#found-user-alias").html("<a href=" + user.html_url + ">" + user.login + "</a>" );
   $("#found-user-join-date").text(user.created_at);
+};
 
+var displayRepos = function(repo) {
+  console.log(repo);
+  if(repo.description === null || repo.description === "") {
+    $("#repo-list").append("<li class='list-group-item' id='repoId"+repo.id+"'>"+repo.name+"</li>");
+
+    $('#repoId'+repo.id).click(function(){
+      $("#found-repo-name").text(repo.name);
+      $("#found-repo-url").html("<a href=" + repo.html_url + ">" + repo.html_url + "</a>");
+      $("#found-repo-description").text("No Description Given");
+    });
+
+  } else {
+    $("#repo-list").append("<li class='list-group-item' id='repoId"+repo.id+"'>"+repo.name+"</li>");
+
+    $('#repoId'+repo.id).click(function(){
+      $("#found-repo-name").text(repo.name);
+      $("#found-repo-url").html("<a href=" + repo.html_url + ">" + repo.html_url + "</a>");
+      $("#found-repo-description").text(repo.description);
+    });
+  }
 };
 
 
@@ -15,6 +35,8 @@ $(document).ready(function(){
     event.preventDefault();
     var currentGitHubSearch = new GitHubSearch();
     var userName = $('#user-name').val();
-    currentGitHubSearch.getRepos(userName, displayUser);
+    $("#user-results").show();
+    $("#repo-descriptions").show();
+    currentGitHubSearch.getUser(userName, displayUser, displayRepos);
   });
 });
--- js/github-search.js
+++ js/github-search.js
@@ -2,14 +2,24 @@ var apiKey = require('./../.env').apiKey;
 
 GitHubSearch = function() {};
 
-GitHubSearch.prototype.getRepos = function(userName, displayUser) {
+GitHubSearch.prototype.getUser = function(userName, displayUser, displayRepos) {
+  var that = this;
   $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
-    console.log(response);
     displayUser(response);
+    that.getRepos(response.login, displayRepos);
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
 };
 
+GitHubSearch.prototype.getRepos = function(userName, displayRepos) {
+  $.get('https://api.github.com/users/' + userName + '/repos?type=owner&sort=created&direction=desc').then(function(response){
+    for(i = 0; i < response.length; i++){
+      displayRepos(response[i]);
+    }
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+};
 
 exports.githubsearchModule = GitHubSearch;
