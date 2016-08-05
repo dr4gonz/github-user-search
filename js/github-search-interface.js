4e384747afde436a3e4c1f3deed107ce2bfdc4d8
Matthew <mreyez@gmail.com>
2016-08-05 09:00:07 -0700

    add basic user information to page after search

--- js/github-search-interface.js
+++ js/github-search-interface.js
@@ -1,10 +1,20 @@
 var GitHubSearch = require('./../js/github-search.js').githubsearchModule;
 
+var displayUser = function(user) {
+  console.log(user);
+  $("#found-user-image").html("<img class='user-avatar-thumb' src="+user.avatar_url+"></img>");
+  $("#found-user-name").text(user.name);
+  $("#found-user-alias").html("<a href=" + user.html_url + ">" + user.login + "</a>" );
+  $("#found-user-join-date").text(user.created_at);
+
+};
+
+
 $(document).ready(function(){
   $("#user-search").submit(function(event){
     event.preventDefault();
     var currentGitHubSearch = new GitHubSearch();
     var userName = $('#user-name').val();
-    currentGitHubSearch.getRepos(userName);
+    currentGitHubSearch.getRepos(userName, displayUser);
   });
 });
--- js/github-search.js
+++ js/github-search.js
@@ -2,12 +2,14 @@ var apiKey = require('./../.env').apiKey;
 
 GitHubSearch = function() {};
 
-GitHubSearch.prototype.getRepos = function(userName) {
+GitHubSearch.prototype.getRepos = function(userName, displayUser) {
   $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
     console.log(response);
+    displayUser(response);
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
 };
+
 
 exports.githubsearchModule = GitHubSearch;
